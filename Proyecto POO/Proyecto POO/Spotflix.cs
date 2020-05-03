f7f9c2d4e2d1c3e8811d0ab7746791e40ccbe4de
juan ignacio roncagliolo <juanignacioroncagliolo@hotmail.com>
2020-05-03 02:23:49 -0400

    ronca

--- Proyecto POO/Proyecto POO/Spotflix.cs
+++ Proyecto POO/Proyecto POO/Spotflix.cs
@@ -9,19 +9,21 @@ namespace Proyecto_POO
 {
     public static class Spotflix
     {
-        // hacer playlist de todas las canciones, de todos los videos, todods los podcast todos los audiolibros etc.
-        //public static string Reproducir_Cancion(Archivo archivo)
-        //{            
-        //}
+        public static Playlist Canciones;
+        public static Playlist Videos;
+        public static Playlist Peliculas;
+        public static Playlist Podcast;
+        public static Playlist Audiolibros;
+
         //Metodos:
-        
-        public static string Reproducir(Playlist playlist)
+
+        public static string Reproducir(Playlist playlist, int v = 0)
         {
             
             SoundPlayer Spotflix = new SoundPlayer();
-            Spotflix.SoundLocation = playlist.Objetos[0].URL;
+            Spotflix.SoundLocation = playlist.Objetos[v].URL;
             Spotflix.Play();
-            string a= "Reproduciendo: "+ playlist.Objetos[0].Titulo;
+            string a= "Reproduciendo: "+ playlist.Objetos[v].Titulo;
             return(a);
         }
         public static string Pausar(Playlist playlist)
