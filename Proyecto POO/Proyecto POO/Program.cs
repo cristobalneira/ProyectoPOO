c69f4e0ea87809c73c4827eb4cff52f195924b34
camilavillalobos1 <civillalobos@miuandes.cl>
2020-05-08 21:33:52 -0400

    Creacion Peliculas

--- Proyecto POO/Proyecto POO/Program.cs
+++ Proyecto POO/Proyecto POO/Program.cs
@@ -23,10 +23,48 @@ namespace Proyecto_POO
         }
         static void Main(string[] args)
         {
-            
+
             //SoundPlayer soundPlayer = new SoundPlayer();
-            
-            //Personas: 
+
+            //Personas:
+
+            //Mas barato por docena
+
+            Persona ShawnLevy = new Persona("Shawn Levy", 1, "Estadounidense", new DateTime(1968, 07,23), new List<int> {4});
+            Persona SteveMartin = new Persona("Steve Martin", 1, "Canadiense", new DateTime(1945, 08, 14), new List<int> { 3 });
+            Persona BonnieHunt = new Persona("Bonnie Hunt", 2, "Estadounidense", new DateTime(1961, 09, 22), new List<int> { 3 });
+            Persona PiperPerabo = new Persona("Piper Perabo", 2, "Estadounidense", new DateTime(1976, 09, 31), new List<int> { 3 });
+
+            //Nerve
+
+            Persona HenryJoost = new Persona("Henry Joost", 1, "Estadounidense", new DateTime(1981, 07, 25), new List<int> { 4 });
+            Persona EmmaRoberts = new Persona("Emma Roberts", 2, "Estadounidense", new DateTime(1991, 02, 10), new List<int> { 3 });
+            Persona DaveFranco = new Persona("Dave Franco", 1, "Estadounidense", new DateTime(1985, 06, 2), new List<int> { 3 });
+            Persona JulietteLewis = new Persona("Juliette Lewis", 2, "Estadounidense", new DateTime(1973, 06, 21), new List<int> { 3 });
+
+            //La Cenicienta
+
+            Persona KennethBranagh = new Persona("Kenneth Branagh", 1, "Britanico", new DateTime(1960, 12, 10), new List<int> {4});
+            Persona LilyJames = new Persona("Lily James", 2, "Britanica", new DateTime(1989, 04, 05), new List<int> { 3 });
+            Persona BenChaplin = new Persona("Ben Chaplin", 1, "Britanico", new DateTime(1969, 07, 31), new List<int> { 3 });
+            Persona RichardMadden = new Persona("Richard Madden", 1, "Britanico", new DateTime(1986, 06, 18), new List<int> { 3 });
+
+            //High School Musical
+
+            Persona KennyOrtega = new Persona("Kenny Ortega", 1, "Estadounidense", new DateTime(1950, 04, 18), new List<int> { 4 });
+            Persona ZacEfron = new Persona("Zac Efron", 1, "Estadounidense", new DateTime(1987, 10, 18), new List<int> { 3 });
+            Persona VanessaHudgens = new Persona("Vanessa Hudgens", 2, "Estadounidense", new DateTime(1988, 12, 14), new List<int> { 3 });
+            Persona AshleyTisdale = new Persona("Ashley Tisdale", 2, "Estadounidense", new DateTime(1985, 07, 02), new List<int> { 3 });
+
+            //Son como niños
+
+            Persona DennisDugan = new Persona("Dennis Dugan", 1, "Estadounidense", new DateTime(1946, 09, 05), new List<int> { 4 });
+            Persona AdamSandler = new Persona("Adam Sandles", 1, "Estadounidense", new DateTime(1966, 09, 09), new List<int> { 3 });
+            Persona KevinJames = new Persona("Kevin James", 1, "Estadounidense", new DateTime(1965, 04, 28), new List<int> { 3 });
+            Persona ChrisRock = new Persona("Chris Rock", 1, "Estadounidense", new DateTime(1967, 02, 13), new List<int> { 3 });
+
+
+
             Persona JustinBieber = new Persona("Justin Bieber", 1, "Canadiense", new DateTime(1996, 03, 01), new List<int> {1,2});
             Persona Adele = new Persona("Adele", 2, "Britanica", new DateTime(), new List<int> { 1, 2 });
             Persona Coolio = new Persona("Coolio", 1, "Estadounidense", new DateTime(1963, 08, 01),new List<int> { 1, 2 });
@@ -49,10 +87,33 @@ namespace Proyecto_POO
                 new List<Persona> { Karol_G, Anuel }, new List<Persona> { Karol_G}, "Keii", "Imagen", "Letra", "Regueton");
             Cancion Skechers = new Cancion("Skechers", 1, new DateTime(2020, 01, 12), TimeSpan.Zero, 10.0, 2,
                 Directory.GetCurrentDirectory() + @"\Musica\DripReport - Skechers.wav",
-                new List<Persona> { DripReport}, new List<Persona> { DripReport }, "Skechers", "Imagen", "Letra", "Pop, Rap, Trap");
+                new List<Persona> { DripReport}, new List<Persona> { DripReport }, "Skechers", "Imagen", "Letra", "Rap");
+
+            //Peliculas
+
+            Pelicula MasBaratoPorDocena = new Pelicula("Mas Barato Por Docena", 1, new DateTime(2003, 12, 25), new TimeSpan(1,46,0), 10.0, 1,
+                "", new List<Persona> {SteveMartin, BonnieHunt, PiperPerabo }, new List<Persona> { ShawnLevy},
+                "‎20th Century Fox Home Entertainment", 1, "Descripcion", 1, "Familiar");
+
+            Pelicula Nerve = new Pelicula("Nerve", 1, new DateTime(2016, 09, 01), new TimeSpan(1,36,0), 10.0, 1,
+                "", new List<Persona> { EmmaRoberts, DaveFranco, JulietteLewis }, new List<Persona> { HenryJoost },
+                "‎Allison Shearmur Productions", 1, "Descripcion", 1, "Accion");
+
+            Pelicula LaCenicienta = new Pelicula("La Cenicienta", 1, new DateTime(2015, 03, 13), new TimeSpan(1, 54, 0), 10.0, 1,
+                "", new List<Persona> { LilyJames, BenChaplin, RichardMadden }, new List<Persona> { KennethBranagh },
+                "Walt Disney Productions", 1, "Descripcion", 1, "Fantasia");
+
+            Pelicula HighSchoolMusical = new Pelicula("High School Musical", 1, new DateTime(2006, 01, 20), new TimeSpan(1, 38, 0), 10.0, 1,
+                "", new List<Persona> {ZacEfron, VanessaHudgens, AshleyTisdale}, new List<Persona> { KennyOrtega },
+                "Salty Films", 1, "Descripcion", 1, "Musical");
+
+            Pelicula SonComoNiños = new Pelicula("Son Como Niños", 1, new DateTime(2010, 06, 24), new TimeSpan(1, 42, 0), 10.0, 1,
+                "", new List<Persona> { AdamSandler, KevinJames, ChrisRock}, new List<Persona> { DennisDugan },
+                "Relativity Media", 1, "Descripcion", 1, "Comedia");
 
 
-            
+
+
             Spotflix.Canciones.Objetos.Add(Gangstas_paradise);
             Spotflix.Canciones.Objetos.Add(Tres_de_abril);
             Spotflix.Canciones.Objetos.Add(Keii);
