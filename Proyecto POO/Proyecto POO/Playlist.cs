13c8e4ed385563f6615b62a64c1f8d236d5e244f
camilavillalobos1 <civillalobos@miuandes.cl>
2020-05-11 23:06:21 -0400

    Buscar

--- Proyecto POO/Proyecto POO/Playlist.cs
+++ Proyecto POO/Proyecto POO/Playlist.cs
@@ -22,12 +22,11 @@ namespace Proyecto_POO
         public Playlist(string Nombre, int tipo_de_playlist)
         {
             Play_list = new WindowsMediaPlayer().newPlaylist(Nombre, null);
-//<<<<<<< HEAD
+
             //Lista_Play_list.Add(Play_list);
-            //=======
             //Lista_Play_list.Add(Play_list);
             //Tipo_De_Playlist = tipo_de_playlist;
-//>>>>>>> 9282fc15fbe368e52cdafe943053befa8c959ce2
+
         }
 
         public string Informacion_Playlist()
